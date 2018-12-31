394ddf257a9be2146e6ef577a876de7a132974b1
EvanXzj <2686465170@qq.com>
2018-12-31 14:05:34 +0800

    链表学习完成

--- src/data-structures-and-algorithms-with-javascript-book/data-structures/linkedlist/loopLinkedlist.js
+++ src/data-structures-and-algorithms-with-javascript-book/data-structures/linkedlist/loopLinkedlist.js
@@ -1,5 +1,8 @@
 /**
- * 链表的简单实现
+ * 循环链表：
+ * 循环链表和单向链表相似， 节点类型一样。唯一的区别就是在创建循环链表的时候让其头节点的next属性指向自身
+ *
+ * 遇到过的面试题： 如何判断一个链表是一个环，即循环链表
  */
 
 class Node {
@@ -9,16 +12,20 @@ class Node {
     }
 }
 
-// 单向列表
-class LinkedList {
+class LoopLinkedList {
     constructor() {
         this.head = new Node('head')
+        this.head.next = this.head
     }
 
     find(element) {
         let currNode = this.head
 
         while(currNode.next && currNode.element !== element) {
+            if (currNode.next.element === 'head') {
+                return currNode
+            }
+
             currNode = currNode.next
         }
 
@@ -36,6 +43,10 @@ class LinkedList {
         let currNode = this.head
 
         while(currNode.next != null && currNode.next.element != element) {
+            if (currNode.next.element === 'head') {
+                return currNode
+            }
+
             currNode = currNode.next
         }
 
@@ -45,7 +56,7 @@ class LinkedList {
     remove(element) {
         const preNode = this.findPrevious(element)
 
-        if (preNode.next != null) {
+        if (preNode.next != null && preNode.next.element !== 'head') {
             preNode.next = preNode.next.next
         }
     }
@@ -53,20 +64,36 @@ class LinkedList {
     display() {
         let currNode = this.head
 
-        while(currNode.next != null) {
+        while(currNode.next != null && currNode.next.element != 'head') {
             console.log(currNode.next.element)
             currNode = currNode.next
         }
     }
+
+    // 循环列表检测
+    isLoopCheck() {
+        let currNode = this.head
+
+        while(currNode.next != null) {
+            if (currNode.element != 'head' && currNode.next.element === 'head') {
+                return true
+            }
+
+            currNode = currNode.next
+        }
+
+        return false
+    }
 }
 
 const log = console.log
-const cities = new LinkedList()
+const cities = new LoopLinkedList()
 cities.insertAfter('Conway', 'head')
-cities.insertAfter('Russelliville', 'Conway')
 cities.insertAfter('Carlisle', 'Russelliville')
 cities.insertAfter('Alma', 'Carlisle')
 cities.display()
-cities.remove('Carlisle')
 log()
+cities.remove('Alma')
 cities.display()
+log(cities.isLoopCheck())
+
